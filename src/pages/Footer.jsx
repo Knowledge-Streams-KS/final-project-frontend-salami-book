6cb1737e329913f7293d08a632239652898e7f42
Mahad9603 <mahadamir9603@gmail.com>
2024-07-26 07:52:07 +0500

    Footer updated

--- src/pages/Footer.jsx
+++ src/pages/Footer.jsx
@@ -1,31 +1,21 @@
 import React from "react";
 import ContactUs from "./ContactUs";
-import { FaSquareInstagram } from "react-icons/fa6";
-import { Link } from "react-router-dom";
-import { FaFacebookSquare } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
 
 const Footer = () => {
   return (
     <>
       <div>
         <ContactUs />
-        <div class="bg-[#08723e] px-4 py-6 md:flex md:items-center md:justify-between dark:bg-gray-700">
-          <span class="text-lg font-normal text-white sm:text-center">
-            © 2023 Copyright 2023 | All Rights Reserved™
-          </span>
-          <div class="mt-4 flex space-x-5 sm:justify-center md:mt-0 rtl:space-x-reverse">
-            <Link to="">
-              <FaFacebookSquare className="text-2xl" />
-            </Link>
-
-            <Link to="">
-              <FaSquareInstagram className="text-2xl" />
-            </Link>
-
-            <Link to="">
-              <FaSquareXTwitter className="text-2xl" />
-            </Link>
+        <div className="flex flex-row justify-between bg-[#08723e] px-6 py-3 text-white">
+          <div>Copyright 2023 | All Rights Reserved</div>
+          <div className="flex flex-row items-center space-x-2">
+            <h1>Follow us</h1>
+            <div><FaInstagram /></div>
+            <div><FaFacebook /></div>
+            <div><FaTiktok /></div>
           </div>
         </div>
       </div>
