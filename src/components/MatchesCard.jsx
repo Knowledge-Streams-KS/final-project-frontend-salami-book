e53e55aa0e9d82df1baa3468c7daa80132103ffc
Mahad9603 <mahadamir9603@gmail.com>
2024-07-26 08:17:56 +0500

    Issues resolved

--- src/components/MatchesCard.jsx
+++ src/components/MatchesCard.jsx
@@ -6,28 +6,28 @@ const MatchesCard = ({ matches, teams, fields }) => {
       <div className="rounded-md bg-[#232727] p-6">
         <div>
           <div className="flex justify-start text-[#08723e]">
-            {teams.find((team) => team.id === matches.team1)?.name}
+            {teams.find((team) => team.id === parseInt(matches.team1))?.name}
           </div>
           <div className="flex justify-center">vs</div>
           <div className="flex justify-end text-[#08723e]">
-            {teams.find((team) => team.id === matches.team2)?.name}
+            {teams.find((team) => team.id === parseInt(matches.team2))?.name}
           </div>
         </div>
         <div className="my-2 border-t-2 border-black"></div>
         <div>
           <div>
             Field Name:{" "}
-            {fields.find((field) => field.id === matches.Booking.FieldId)?.name}
+            {fields.find((field) => field.id === parseInt(matches.FieldId))?.name}
           </div>
-          <div>Match day: {matches.Booking.date}</div>
-          <div>Match Time: {matches.Booking.time}</div>
+          <div>Match day: {matches.Bookings[0].date}</div>
+          <div>Match Time: {matches.Bookings[0].time}</div>
         </div>
         {matches.completed ? (
           <div className="space-y-1 py-2 text-center text-[#08723e]">
             {matches.team1Score > matches.team2Score ? (
-              <h1>Team 1 Won</h1>
+              <h1>{teams.find((team) => team.id === parseInt(matches.team1))?.name} Won</h1>
             ) : matches.team2Score > matches.team1Score ? (
-              <h1>Team 2 Won</h1>
+              <h1>{teams.find((team) => team.id === parseInt(matches.team2))?.name} Won</h1>
             ) : (
               <h1>Draw</h1>
             )}
